a085e3aac9f35b80295766aef04cc4af975c4822
Harsh <soni.hs661@gmail.comm>
2022-05-11 22:20:51 +0530

    login and signup event handler,payload,input field

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,32 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import {TextField,Button} from '@mui/material'
 function Login(props) {
+    const [userDetails, setUserDetails] = useState({
+        email: "",
+        password: "",
+       
+      });
+      function handleChange(event) {
+        const { name, value } = event.target;
+        setUserDetails({
+          ...userDetails,
+          [name]: value,
+        });
+      }
+      function handleSubmit(event) {
+        event.preventDefault();
+    
+        const { email, password} = userDetails;
+        console.log(userDetails);
+      }
     return (
-        <div>
-            this is login page
+        <div className="login">
+        <div className='login-form'></div>
+            <TextField label="email" name="email" value={userDetails.email} color="secondary" focused onChange={handleChange}/>
+            <TextField label="password" name="password" value={userDetails.password} color="secondary" focused onChange={handleChange}/>
+            <Button variant="contained" onClick={handleSubmit}>
+         Login
+        </Button>
         </div>
     );
 }
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,11 +1,70 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 function Signup(props) {
-    return (
-        <div>
-            this is signup page
-        </div>
-    );
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUserDetails({
+      ...userDetails,
+      [name]: value,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const { name, email, password, confirm_password } = userDetails;
+    if ((name, email && password === confirm_password)) {
+      console.log(userDetails);
+    } else {
+      console.log("password and confirmpassword dont match");
+    }
+  }
+  return (
+    <div className="signup">
+      <div className="signup-form">
+        <TextField
+          label="name"
+          name="name"
+          value={userDetails.name}
+          color="secondary"
+          focused
+          onChange={handleChange}
+        />
+        <TextField
+          label="email"
+          name="email"
+          value={userDetails.email}
+          color="secondary"
+          focused
+          onChange={handleChange}
+        />
+        <TextField
+          label="password"
+          name="password"
+          value={userDetails.password}
+          color="secondary"
+          focused
+          onChange={handleChange}
+        />
+        <TextField
+          label="confirm Password"
+          name="confirm_password"
+          value={userDetails.confirm_password}
+          color="secondary"
+          focused
+          onChange={handleChange}
+        />
+        <Button variant="contained" onClick={handleSubmit}>
+         Signup
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
